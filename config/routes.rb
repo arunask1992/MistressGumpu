529524151c2f43f085c5f75299d625395a37febb
Aruna Shanmugakumar <arunash@thoughtworks.com>
2016-03-08 13:05:31 +0530

    Google auth some more tweak

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: 'home#index'
   get '/auth/:provider/callback', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308061443) do
+ActiveRecord::Schema.define(version: 20160308070439) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -38,10 +38,18 @@ ActiveRecord::Schema.define(version: 20160308061443) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
     t.string   "name"
-    t.string   "email"
+    t.string   "location"
+    t.string   "image_url"
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
+  add_index "users", ["provider"], name: "index_users_on_provider"
+  add_index "users", ["uid"], name: "index_users_on_uid"
 
 end
