abebcacb4a9d3c08b7ffd398f9281f06f0646999
Sajani <deepasaj@thoughtworks.com>
2016-03-08 16:39:26 +0530

    Plot map only if element is present

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,10 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$('.eventsmap').ready(function() {
+$('#eventsmap').ready(function () {
     var current_lat = 12.8844681;
     var current_long = 77.6377612;
-    var mymap = L.map('eventsmap').setView([current_lat,current_long], 13);
+    if ($('#eventsmap').size() == 0) return;
+    var mymap = L.map('eventsmap').setView([current_lat, current_long], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoibWFkaHVtaXRoYXAiLCJhIjoiY2lsajFxNDJnM3dnZ3Y2bTAzNDF2bDh1byJ9.1CrFHATHc4-BD2F69nfTRw'
@@ -15,7 +16,7 @@ $('.eventsmap').ready(function() {
         url: '/events_list',
         type: 'get',
         success: function (data) {
-            for (i = 0; i < data.length; i++) { 
+            for (i = 0; i < data.length; i++) {
                 my_event = data[i];
                 add_marker_with_icon(mymap, my_event.venue_latitude, my_event.venue_logitude, "<b>".concat(my_event.title).concat("</b>"));
             }
@@ -23,18 +24,18 @@ $('.eventsmap').ready(function() {
     });
 });
 
-var add_marker_with_icon = function(map, lat, long, popup) {
+var add_marker_with_icon = function (map, lat, long, popup) {
     var marker_icon = L.icon({
         iconUrl: '/assets/cal.png',
-        iconSize:     [30, 27], 
-        iconAnchor:   [22, 94], 
-        popupAnchor:  [-3, -76]
+        iconSize: [30, 27],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76]
     });
     var marker = L.marker([lat, long], {icon: marker_icon}).addTo(map);
     bind_popup(marker, popup);
 };
 
-var bind_popup = function(marker, popup) {
+var bind_popup = function (marker, popup) {
     marker.bindPopup(popup);
     marker.on('mouseover', function (e) {
         this.openPopup();
