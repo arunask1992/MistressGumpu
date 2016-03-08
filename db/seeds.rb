6c0e4cec7a957c37b6ff76310225db57ce56b7a3
Aruna Shanmugakumar <arunash@thoughtworks.com>
2016-03-08 11:29:53 +0530

    Some routes and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+genres = Genre.create([{name: 'Spiritual Discourses'},{name: 'Art'}, {name: 'Crafts'}, {name: 'French'}])
